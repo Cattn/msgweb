b61e31787cc33988d0c7181eaeb1063b97ee0c38
Logan <69491394+Cattn@users.noreply.github.com>
2023-02-02 19:06:33 +0000

    Added Stuff

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ window.onscroll = function() {scrollFunction()};
 const navLinks = document.querySelectorAll(".link");
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("nav").style.padding = "1vw 0.5vw";
+  if (document.body.scrollTop > 235 || document.documentElement.scrollTop > 235) {
+    _.setCSS("#nav", "padding", "1vw 0.3vw");
    navLinks.forEach(navLinks => {
-     navLinks.style.fo
+     navLinks.style.fontSize = "1.3vw"
    })
+    _.setCSS("#logoMain", "height", "3vw");
   } else {
-    document.getElementById("nav").style.padding = "10vw 0.5vw";
+    _.setCSS("#nav", "padding", "10vw 0.5vw");
+    navLinks.forEach(navLinks => {
+     navLinks.style.fontSize = "2vw"
+   })
+    _.setCSS("#logoMain", "height", "15vw");
   }
 }
