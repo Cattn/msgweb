6eb15f5e316dd6964cc2102d494c6c8a5f3616a7
Logan <thecrazykid2121@gmail.com>
2023-02-11 17:03:44 -0500

    push

--- games/games.js
+++ games/games.js
@@ -7,31 +7,31 @@ var games = [
     "file": "/games/files/slope"
   },
   {
-    "id": "slope2",
-    "title": "Slope",
-    "image": "https://play-lh.googleusercontent.com/uJn2i9h7KxYQarC_c3K4qH6o7gLtflFnhD_dN14MNkzHJ1NeNFzCL69jpB5mT0vCoQs=w240-h480-rw",
-    "description": "Play Slope Game, an endless space run game. Drive a ball in the 3D running game in Slope Game. Easy to control, high speed, and addictive gameplay",
-    "file": "/games/files/slope"
+    "id": "1",
+    "title": "1",
+    "image": "1/meta/og_image.png",
+    "description": "Divide the tiles to get to 1!",
+    "file": "../3kh0-Assets/1"
   },
     {
     "id": "slope3",
-    "title": "Slope",
-    "image": "https://play-lh.googleusercontent.com/uJn2i9h7KxYQarC_c3K4qH6o7gLtflFnhD_dN14MNkzHJ1NeNFzCL69jpB5mT0vCoQs=w240-h480-rw",
+    "title": "100ng",
+    "image": "../3kh0-Assets/100ng/100ng.png",
     "description": "Play Slope Game, an endless space run game. Drive a ball in the 3D running game in Slope Game. Easy to control, high speed, and addictive gameplay",
-    "file": "/games/files/slope"
+    "file": "../3kh0-Assets/100ng"
   },
   {
     "id": "slope4",
-    "title": "Slope",
+    "title": "1v1.lol",
     "image": "https://play-lh.googleusercontent.com/uJn2i9h7KxYQarC_c3K4qH6o7gLtflFnhD_dN14MNkzHJ1NeNFzCL69jpB5mT0vCoQs=w240-h480-rw",
     "description": "Play Slope Game, an endless space run game. Drive a ball in the 3D running game in Slope Game. Easy to control, high speed, and addictive gameplay",
-    "file": "/games/files/slope"
+    "file": "../3kh0-Assets/1v1lol"
   },
   {
     "id": "slope5",
-    "title": "Slope",
+    "title": "1v1-Space",
     "image": "https://play-lh.googleusercontent.com/uJn2i9h7KxYQarC_c3K4qH6o7gLtflFnhD_dN14MNkzHJ1NeNFzCL69jpB5mT0vCoQs=w240-h480-rw",
     "description": "Play Slope Game, an endless space run game. Drive a ball in the 3D running game in Slope Game. Easy to control, high speed, and addictive gameplay",
-    "file": "/games/files/slope"
+    "file": "../3kh0-Assets/1v1space"
   }
 ]
