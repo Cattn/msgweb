9d4d4bffb9d5299c85dc5bdc634a4da29fa0d4c0
Logan <thecrazykid2121@gmail.com>
2023-02-14 09:05:10 -0500

    prob brick site styling

--- script.js
+++ script.js
@@ -4,99 +4,50 @@ const linkBack = document.querySelectorAll(".linkBack");
 
 function scrollFunction() {
   if (window.matchMedia("(max-width: 640px)").matches == false) {
-  if (document.body.scrollTop > 235 || document.documentElement.scrollTop > 235) {
-    _.setCSS("#nav", "padding", "2.5vw 0.3vw");
+  if (document.body.scrollTop > 235 || document.documentElement.scrollTop > 100) {
+    _.setCSS("#nav", "padding", "1vw 0.1vw");
    navLinks.forEach(navLinks => {
-     navLinks.style.fontSize = "2vw"
-     navLinks.style.height = "2vw"
+     navLinks.style.fontSize = "1vw"
+     navLinks.style.height = "1vw"
       navLinks.style.width = "7vw"
       navLinks.style.paddingTop = "0.3vw"
       navLinks.style.marginLeft = "0.3vw"
 
    })
    linkBack.forEach(linkBack => {
-    linkBack.style.height = "3vw"
+    linkBack.style.height = "2vw"
     linkBack.style.width = "9vw"
   })
-    _.setCSS("#logoMain", "height", "5vw");
+    _.setCSS("#logoMain", "display", "none");
     _.setCSS("#logoMain", "margin-left", "5vw");
-
-    // Set Media Sizes 
-    _.setCSS("#mediaPlayerOuter", "margin-bottom", "1vh");
-    _.setCSS("#mediaPlayerOuter", "margin-left", "53vw");
-
-    _.setCSS("#mediaPlayerOuter", "height", "11vh");
-    _.setCSS("#mediaPlayerOuter", "width", "20vw");
-
-    function checkForSmall(x) {
-      if (x.matches) { // If media query matches
-        document.getElementById("mediaPlayerWrap").style.marginTop = "0vh";
-      } else {
-        document.getElementById("mediaPlayerWrap").style.marginTop = "1vh";
-      }
-    }
+    _.setCSS("#spaceDesktop2", "width", "30%");
     
-    var x = window.matchMedia("(max-width: 900px)");
-    checkForSmall(x);
-    x.addListener(checkForSmall);
-
-    function lowerSize(x) {
-      if (x.matches) { // If media query matches
-        document.getElementById("backArrow").style.height = "2vw";
-        document.getElementById("backArrow").style.width = "2vw";
-        document.getElementById("backArrow").style.borderRadius = "2vh";
-
-        document.getElementById("pause").style.height = "2vw";
-        document.getElementById("pause").style.width = "2vw";
-        document.getElementById("pause").style.borderRadius = "2vh";
-
-        document.getElementById("forwardArrow").style.height = "2vw";
-        document.getElementById("forwardArrow").style.width = "2vw";
-        document.getElementById("forwardArrow").style.borderRadius = "2vh";
-      } else {
-        document.getElementById("backArrow").style.height = "3vw";
-        document.getElementById("backArrow").style.width = "3vw";
-        document.getElementById("backArrow").style.borderRadius = "3vh";
-
-        document.getElementById("pause").style.height = "3vw";
-        document.getElementById("pause").style.width = "3vw";
-        document.getElementById("pause").style.borderRadius = "3vh";
-
-        document.getElementById("forwardArrow").style.height = "3vw";
-        document.getElementById("forwardArrow").style.width = "3vw";
-        document.getElementById("forwardArrow").style.borderRadius = "3vh";
-      }
-    }
-    
-    var x = window.matchMedia("(max-width: 900px)");
-    lowerSize(x);
-    x.addListener(lowerSize);
     // Set Quick Setting Sizes
     _.setCSS("#quickSettingsOuter", "margin-top", "0vh");
-    _.setCSS("#quickSettingsOuter", "margin-left", "74vw");
+    _.setCSS("#quickSettingsOuter", "margin-left", "80vw");
 
 
-    _.setCSS("#quickSettingsOuter", "height", "11vh");
+    _.setCSS("#quickSettingsOuter", "height", "8vh");
     _.setCSS("#quickSettingsOuter", "width", "20vw");
 
-    _.setCSS("#quickSettingsWrap", "margin-top", "1.5vh");
+    _.setCSS("#quickSettingsWrap", "margin-top", "1vh");
 
-    _.setCSS("#themeSwitch", "height", "4vw");
-    _.setCSS("#themeSwitch", "width", "4vw");
-    _.setCSS("#themeSwitch", "border-radius", "3vh");
+    _.setCSS("#themeSwitch", "height", "2.8vw");
+    _.setCSS("#themeSwitch", "width", "2.8vw");
+    _.setCSS("#themeSwitch", "border-radius", "2vh");
     _.setCSS("#themeSwitch", "margin-right", "1vw");
 
-    _.setCSS("#reloadSwitch", "height", "4vw");
-    _.setCSS("#reloadSwitch", "width", "4vw");
-    _.setCSS("#reloadSwitch", "border-radius", "3vh");
+    _.setCSS("#reloadSwitch", "height", "2.8vw");
+    _.setCSS("#reloadSwitch", "width", "2.8vw");
+    _.setCSS("#reloadSwitch", "border-radius", "2vh");
     _.setCSS("#reloadSwitch", "margin-right", "1vw");
 
-    _.setCSS("#fullScreenSwitch", "height", "4vw");
-    _.setCSS("#fullScreenSwitch", "width", "4vw");
-    _.setCSS("#fullScreenSwitch", "border-radius", "3vh");
+    _.setCSS("#fullScreenSwitch", "height", "2.8vw");
+    _.setCSS("#fullScreenSwitch", "width", "2.8vw");
+    _.setCSS("#fullScreenSwitch", "border-radius", "2vh");
     _.setCSS("#fullScreenSwitch", "margin-right", "1vw");
   } else {
-    _.setCSS("#nav", "padding", "10vw 0.5vw");
+    _.setCSS("#nav", "padding", "2vw 0.5vw");
     navLinks.forEach(navLinks => {
      navLinks.style.fontSize = "2vw"
      navLinks.style.height = "5vw"
@@ -108,43 +59,13 @@ function scrollFunction() {
    linkBack.style.height = "9vh"
    linkBack.style.width = "12%"
  })
-   _.setCSS("#logoMain", "height", "15vw");
+   _.setCSS("#logoMain", "display", "none");
    _.setCSS("#logoMain", "margin-left", "1vw");
-
-    // Re-size Media
-    _.setCSS("#mediaPlayerOuter", "margin-bottom", "20vh");
-    _.setCSS("#mediaPlayerOuter", "margin-left", "70vw");
-
-    function checkForBig(x) {
-      if (x.matches) { // If media query matches
-        document.getElementById("mediaPlayerWrap").style.marginTop = "1vh";
-      } else {
-        document.getElementById("mediaPlayerWrap").style.marginTop = "5vh";
-      }
-    }
-    
-    var x = window.matchMedia("(max-width: 1500px)");
-    checkForBig(x);
-    x.addListener(checkForBig);
-
-    _.setCSS("#mediaPlayerOuter", "height", "18vh");
-    _.setCSS("#mediaPlayerOuter", "width", "23vw");
-
-    _.setCSS("#backArrow", "height", "4vw");
-    _.setCSS("#backArrow", "width", "4vw");
-    _.setCSS("#backArrow", "border-radius", "2vh");
-
-    _.setCSS("#pause", "height", "4vw");
-    _.setCSS("#pause", "width", "4vw");
-    _.setCSS("#pause", "border-radius", "2vh");
-
-    _.setCSS("#forwardArrow", "height", "4vw");
-    _.setCSS("#forwardArrow", "width", "4vw");
-    _.setCSS("#forwardArrow", "border-radius", "2vh");
+   _.setCSS("#spaceDesktop2", "width", "20%");
 
     // Re-Size Quick Setting Sizes
-    _.setCSS("#quickSettingsOuter", "margin-top", "22vh");
-    _.setCSS("#quickSettingsOuter", "margin-left", "70vw");
+    _.setCSS("#quickSettingsOuter", "margin-top", "4vh");
+    _.setCSS("#quickSettingsOuter", "margin-left", "75vw");
 
 
     _.setCSS("#quickSettingsOuter", "height", "18vh");
@@ -152,18 +73,18 @@ function scrollFunction() {
 
     _.setCSS("#quickSettingsWrap", "margin-top", "3vh");
 
-    _.setCSS("#themeSwitch", "height", "6vw");
-    _.setCSS("#themeSwitch", "width", "6vw");
+    _.setCSS("#themeSwitch", "height", "4vw");
+    _.setCSS("#themeSwitch", "width", "4vw");
     _.setCSS("#themeSwitch", "border-radius", "2vh");
     _.setCSS("#themeSwitch", "margin-right", "0.5vw");
 
-    _.setCSS("#reloadSwitch", "height", "6vw");
-    _.setCSS("#reloadSwitch", "width", "6vw");
+    _.setCSS("#reloadSwitch", "height", "4vw");
+    _.setCSS("#reloadSwitch", "width", "4vw");
     _.setCSS("#reloadSwitch", "border-radius", "2vh");
     _.setCSS("#reloadSwitch", "margin-right", "0.5vw");
 
-    _.setCSS("#fullScreenSwitch", "height", "6vw");
-    _.setCSS("#fullScreenSwitch", "width", "6vw");
+    _.setCSS("#fullScreenSwitch", "height", "4vw");
+    _.setCSS("#fullScreenSwitch", "width", "4vw");
     _.setCSS("#fullScreenSwitch", "border-radius", "2vh");
     _.setCSS("#fullScreenSwitch", "margin-right", "0.5vw");
   }
@@ -505,6 +426,7 @@ document.getElementById("songData").addEventListener("click", function(event) {
   playSong(songs[currentSongIndex]);
 });
 
+/*
 document.getElementById("volumeControl").addEventListener("input", function(event) {
   if (aud) {
     aud.volume = event.target.value;
@@ -546,7 +468,7 @@ pause.addEventListener("click", function() {
     isPlaying = true;
   }
 });
-
+*/
 lastSentTime = 0;
 
 function sendMessage() {
