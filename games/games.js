54bcdd55f3682213513c382024626f4304697e2f
Logan <thecrazykid2121@gmail.com>
2023-02-25 18:21:44 -0500

    Absolute Links (EW!!!! I'll make it dynamic later)

--- games/games.js
+++ games/games.js
@@ -9,29 +9,29 @@ var games = [
   {
     "id": "1",
     "title": "1",
-    "image": "../../3kh0-Assets/1/meta/og_image.png",
+    "image": "https://cattn.github.io/3kh0-Assets/1/meta/og_image.png",
     "description": "Divide the tiles to get to 1!",
-    "file": "../../3kh0-Assets/1"
+    "file": "https://cattn.github.io/3kh0-Assets/1"
   },
     {
     "id": "slope3",
     "title": "100ng",
-    "image": "../../3kh0-Assets/100ng/100ng.jpg",
+    "image": "https://cattn.github.io/3kh0-Assets/100ng/100ng.jpg",
     "description": "Pong, but with some sorta twist.",
-    "file": "../../3kh0-Assets/100ng"
+    "file": "https://cattn.github.io/3kh0-Assets/100ng"
   },
   {
     "id": "slope4",
     "title": "1v1.lol",
     "image": "https://play-lh.googleusercontent.com/uJn2i9h7KxYQarC_c3K4qH6o7gLtflFnhD_dN14MNkzHJ1NeNFzCL69jpB5mT0vCoQs=w240-h480-rw",
     "description": "Play Slope Game, an endless space run game. Drive a ball in the 3D running game in Slope Game. Easy to control, high speed, and addictive gameplay",
-    "file": "../../3kh0-Assets/1v1lol"
+    "file": "https://cattn.github.io/3kh0-Assets/1v1lol"
   },
   {
     "id": "slope5",
     "title": "1v1-Space",
     "image": "https://play-lh.googleusercontent.com/uJn2i9h7KxYQarC_c3K4qH6o7gLtflFnhD_dN14MNkzHJ1NeNFzCL69jpB5mT0vCoQs=w240-h480-rw",
     "description": "Play Slope Game, an endless space run game. Drive a ball in the 3D running game in Slope Game. Easy to control, high speed, and addictive gameplay",
-    "file": "../../3kh0-Assets/1v1space"
+    "file": "https://cattn.github.io/3kh0-Assets/1v1space"
   }
 ]
