a37e40055098238b3c68edbdbeb0ca595ba4e039
Logan <thecrazykid2121@gmail.com>
2023-03-03 20:29:00 -0500

    bruh

--- games/games.js
+++ games/games.js
@@ -2,185 +2,185 @@ var games = [
     {
         "id": "1",
         "title": "1",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/1/1.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/1/1.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/1/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/1/index.html"
     },
     
     {
         "id": "2048",
         "title": "2048",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/2048/2048.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/2048/2048.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/2048/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/2048/index.html"
     },
     
     {
         "id": "among-us",
         "title": "among-us",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/among-us/among-us.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/among-us/among-us.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/among-us/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/among-us/index.html"
     },
     
     {
         "id": "bloonstd",
         "title": "bloonstd",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/bloonstd/bloonstd.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/bloonstd/bloonstd.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/bloonstd/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/bloonstd/index.html"
     },
     
     {
         "id": "bloonstd2",
         "title": "bloonstd2",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/bloonstd2/bloonstd2.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/bloonstd2/bloonstd2.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/bloonstd2/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/bloonstd2/index.html"
     },
     
     {
         "id": "cookie-clicker",
         "title": "cookie-clicker",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/cookie-clicker/cookie-clicker.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/cookie-clicker/cookie-clicker.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/cookie-clicker/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/cookie-clicker/index.html"
     },
     
     {
         "id": "ctr",
         "title": "ctr",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/ctr/ctr.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr/ctr.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/ctr/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr/index.html"
     },
     
     {
         "id": "ctr-holiday",
         "title": "ctr-holiday",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/ctr-holiday/ctr-holiday.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr-holiday/ctr-holiday.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/ctr-holiday/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr-holiday/index.html"
     },
     
     {
         "id": "ctr-tr",
         "title": "ctr-tr",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/ctr-tr/ctr-tr.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr-tr/ctr-tr.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/ctr-tr/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr-tr/index.html"
     },
     
     {
         "id": "deal-or-no-deal",
         "title": "deal-or-no-deal",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/deal-or-no-deal/deal-or-no-deal.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/deal-or-no-deal/deal-or-no-deal.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/deal-or-no-deal/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/deal-or-no-deal/index.html"
     },
     
     {
         "id": "filenames.txt",
         "title": "filenames.txt",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/filenames.txt/filenames.txt.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/filenames.txt/filenames.txt.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/filenames.txt/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/filenames.txt/index.html"
     },
     
     {
         "id": "flappybird",
         "title": "flappybird",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/flappybird/flappybird.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/flappybird/flappybird.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/flappybird/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/flappybird/index.html"
     },
     
     {
         "id": "geodash",
         "title": "geodash",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/geodash/geodash.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/geodash/geodash.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/geodash/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/geodash/index.html"
     },
     
     {
         "id": "idle-breakout",
         "title": "idle-breakout",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/idle-breakout/idle-breakout.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/idle-breakout/idle-breakout.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/idle-breakout/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/idle-breakout/index.html"
     },
     
     {
         "id": "impossiblequiz",
         "title": "impossiblequiz",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/impossiblequiz/impossiblequiz.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/impossiblequiz/impossiblequiz.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/impossiblequiz/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/impossiblequiz/index.html"
     },
     
     {
         "id": "jetpack-joyride",
         "title": "jetpack-joyride",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/jetpack-joyride/jetpack-joyride.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/jetpack-joyride/jetpack-joyride.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/jetpack-joyride/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/jetpack-joyride/index.html"
     },
     
     {
         "id": "learntofly",
         "title": "learntofly",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/learntofly/learntofly.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/learntofly/learntofly.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/learntofly/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/learntofly/index.html"
     },
     
     {
         "id": "learntofly2",
         "title": "learntofly2",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/learntofly2/learntofly2.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/learntofly2/learntofly2.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/learntofly2/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/learntofly2/index.html"
     },
     
     {
         "id": "slope",
         "title": "slope",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/slope/slope.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/slope/slope.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/slope/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/slope/index.html"
     },
     
     {
         "id": "stealingthediamond",
         "title": "stealingthediamond",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/stealingthediamond/stealingthediamond.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/stealingthediamond/stealingthediamond.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/stealingthediamond/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/stealingthediamond/index.html"
     },
     
     {
         "id": "temple-run-2",
         "title": "temple-run-2",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/temple-run-2/temple-run-2.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/temple-run-2/temple-run-2.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/temple-run-2/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/temple-run-2/index.html"
     },
     
     {
         "id": "wordle",
         "title": "wordle",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/wordle/wordle.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/wordle/wordle.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/wordle/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/wordle/index.html"
     },
     
     {
         "id": "you-are-bezos",
         "title": "you-are-bezos",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/you-are-bezos/you-are-bezos.jpg",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/you-are-bezos/you-are-bezos.jpg",
         "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/tree/main/you-are-bezos/index.html"
+        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/you-are-bezos/index.html"
     }
     
     
