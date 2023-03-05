da6f59ea877eaa8850d574c5907a99dd46ebaf69
Logan <thecrazykid2121@gmail.com>
2023-03-04 21:25:36 -0500

    Games Fully Fixed + Updated

--- games/games.js
+++ games/games.js
@@ -2,24 +2,24 @@ var games = [
     {
         "id": "1",
         "title": "1",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/1/1.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/1/meta/og_image.png",
+        "description": "Divide the squares to reach 1! Starting at 2048, this game essentially reverses the rules of 2048. You start with 2048 and must combine the squares to reach 1. The game ends when you reach 1 or when you run out of moves.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/1/index.html"
     },
     
     {
         "id": "2048",
         "title": "2048",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/2048/2048.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/2048/2048.png",
+        "description": "Join the numbers and get to the 2048 tile! Combine the numbers to get to the 2048 tile. You can combine two tiles by swiping in any direction. When two tiles with the same number touch, they merge into one!",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/2048/index.html"
     },
     
     {
         "id": "among-us",
         "title": "among-us",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/among-us/among-us.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/among-us/red.png",
+        "description": "A fan game of the popular game Among Us.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/among-us/index.html"
     },
     
@@ -27,127 +27,119 @@ var games = [
         "id": "bloonstd",
         "title": "bloonstd",
         "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/bloonstd/bloonstd.jpg",
-        "description": "",
+        "description": "Defend yourself from the ballons by popping them and buying upgrades.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/bloonstd/index.html"
     },
     
     {
         "id": "bloonstd2",
         "title": "bloonstd2",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/bloonstd2/bloonstd2.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/bloonstd2/bloonstd2.png",
+        "description": "Defend yourself from the ballons by popping them and buying upgrades. This is the sequel to bloonstd.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/bloonstd2/index.html"
     },
     
     {
         "id": "cookie-clicker",
         "title": "cookie-clicker",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/cookie-clicker/cookie-clicker.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/cookie-clicker/cookie1.jpeg",
+        "description": "Click the cookie to get more cookies. You can buy upgrades with the cookies you get. You can also buy buildings that will automatically give you cookies.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/cookie-clicker/index.html"
     },
     
     {
         "id": "ctr",
         "title": "ctr",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr/ctr.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr/logo.png",
+        "description": "Cut the rope to feed the monster. ",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr/index.html"
     },
     
     {
         "id": "ctr-holiday",
         "title": "ctr-holiday",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr-holiday/ctr-holiday.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr-holiday/Holiday_Gift.webp",
+        "description": "Cut the rope to feed the monster. This is a holiday version of ctr.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr-holiday/index.html"
     },
     
     {
         "id": "ctr-tr",
         "title": "ctr-tr",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr-tr/ctr-tr.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr-tr/logo.png",
+        "description": "Cut the rope to feed the monster. This is a Time Travel version of ctr.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/ctr-tr/index.html"
     },
     
     {
         "id": "deal-or-no-deal",
         "title": "deal-or-no-deal",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/deal-or-no-deal/deal-or-no-deal.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/deal-or-no-deal/index.jpg",
+        "description": "Choose a box and see if you get the most money. You can also choose to take the deal. If you don't like the deal, you can choose to open another box.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/deal-or-no-deal/index.html"
-    },
-    
-    {
-        "id": "filenames.txt",
-        "title": "filenames.txt",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/filenames.txt/filenames.txt.jpg",
-        "description": "",
-        "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/filenames.txt/index.html"
     },
     
     {
         "id": "flappybird",
         "title": "flappybird",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/flappybird/flappybird.jpg",
-        "description": "",
+        "image": "https://upload.wikimedia.org/wikipedia/en/0/0a/Flappy_Bird_icon.png",
+        "description": "Fly through the pipes. If you hit a pipe, you lose. If you hit the ground, you lose. If you hit the ceiling, you lose.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/flappybird/index.html"
     },
     
     {
         "id": "geodash",
         "title": "geodash",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/geodash/geodash.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/geodash/geoscratchicon.png",
+        "description": "Geometry Dash but in Scratch.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/geodash/index.html"
     },
     
     {
         "id": "idle-breakout",
         "title": "idle-breakout",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/idle-breakout/idle-breakout.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/idle-breakout/img/thumbnail.png",
+        "description": "Breakout but you don't have to play. You can just leave it running and it will play for you.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/idle-breakout/index.html"
     },
     
     {
         "id": "impossiblequiz",
         "title": "impossiblequiz",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/impossiblequiz/impossiblequiz.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/impossiblequiz/impossiblequiz.png",
+        "description": "The Impossible Quiz. Answer the questions correctly to win. If you get a question wrong, you lose.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/impossiblequiz/index.html"
     },
     
     {
         "id": "jetpack-joyride",
         "title": "jetpack-joyride",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/jetpack-joyride/jetpack-joyride.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/jetpack-joyride/splash.jpg",
+        "description": " Jetpack Joyride. Fly through the levels and collect coins. If you hit a wall, you lose. If you hit a laser, you lose.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/jetpack-joyride/index.html"
     },
     
     {
         "id": "learntofly",
         "title": "learntofly",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/learntofly/learntofly.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/learntofly/learntofly.png",
+        "description": "Learn to Fly, learn to fly as a penguin.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/learntofly/index.html"
     },
     
     {
         "id": "learntofly2",
         "title": "learntofly2",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/learntofly2/learntofly2.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/learntofly2/learn-to-fly-2.jpg",
+        "description": "Learn to Fly 2, learn to fly as a penguin. This time, you can fly better.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/learntofly2/index.html"
     },
     
     {
         "id": "slope",
         "title": "slope",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/slope/slope.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/slope/slope4.jpeg",
+        "description": "Slope, a game where you have to dodge obstacles.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/slope/index.html"
     },
     
@@ -162,24 +154,24 @@ var games = [
     {
         "id": "temple-run-2",
         "title": "temple-run-2",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/temple-run-2/temple-run-2.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/temple-run-2/img/icons/icon-512x512.png",
+        "description": "Temple Run 2, run through the temple and collect coins.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/temple-run-2/index.html"
     },
     
     {
         "id": "wordle",
         "title": "wordle",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/wordle/wordle.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/wordle/img/logo_512x512.png",
+        "description": "Wordle, a game where you have to guess the word.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/wordle/index.html"
     },
     
     {
         "id": "you-are-bezos",
         "title": "you-are-bezos",
-        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/you-are-bezos/you-are-bezos.jpg",
-        "description": "",
+        "image": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/you-are-bezos/img/banner.png",
+        "description": "You are Bezos, a game where you have to be the richest person in the world.",
         "file": "https://rawcdn.githack.com/mathlover1/msgweb-assets/a077ab435fe0032e8f156bd4ca68b6abb64ce640/you-are-bezos/index.html"
     }
     
